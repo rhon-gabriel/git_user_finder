d231e37f3be6058fb27858318a851fd954dde924
Rhoneil Gabriel <rhoneilgabriel@Rhoneils-MacBook-Air.local>
2021-11-28 13:59:03 +0100

    initialize create react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Github User Finder
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
